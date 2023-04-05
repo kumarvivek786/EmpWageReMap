76d65457cf9011dbba3caa4302d2cc76e3547460
vivek <ikumarvivek786@gmail.com>
2023-04-05 19:36:10 +0530

    [Vivek]Employee Wage Using Switch Statement]

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -11,28 +11,26 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation");
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            const int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            if(empCheck == IS_PART_TIME)
+            int empCheck = random.Next(0,3);
+            switch (empCheck)
             {
-                empHrs = 4;
-                Console.WriteLine("PartTime Employee is Present");
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-                Console.WriteLine("FullTIme Employee is Present");
-
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is Absent");
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    Console.WriteLine("PartTime Employee is Present");
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    Console.WriteLine("FullTime Employee is Present");
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee wages is=" + empWage);
