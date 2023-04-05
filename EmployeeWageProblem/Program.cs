f6404e78d9ad13b6d19dcfd63130cfe83713e6d5
vivek <ikumarvivek786@gmail.com>
2023-04-05 16:47:28 +0530

    [Vivek]Employee Part Time]

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -11,19 +11,28 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation");
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if(empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+                Console.WriteLine("PartTime Employee is Present");
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+                Console.WriteLine("FullTIme Employee is Present");
+
             }
             else
             {
                 empHrs = 0;
+                Console.WriteLine("Employee is Absent");
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee wages is=" + empWage);
